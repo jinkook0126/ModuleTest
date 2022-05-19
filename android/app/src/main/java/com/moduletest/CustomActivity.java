df950fce73c8c2eabb2df7617a3025cb213be42c
jinqook0126 <jinqook0126@gmail.com>
2022-05-20 00:17:21 +0900

    feat: 네이티브 스크린 이동 및 종료

--- android/app/src/main/java/com/moduletest/CustomActivity.java
+++ android/app/src/main/java/com/moduletest/CustomActivity.java
@@ -3,6 +3,9 @@ package com.moduletest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class CustomActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class CustomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
